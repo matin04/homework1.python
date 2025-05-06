92e55b0c6f2448467f0b589693cd8faa190b775b
matin04 <fathuddinzodamatin@gmail.com>
2025-05-06 18:22:59 +0300

    1.4.9

--- w.kh.py
+++ w.kh.py
@@ -1,11 +1,11 @@
-# test1.4.8 /////////////////////////
+# test1.4.9 /////////////////////////
 
 
 a=int(input())
 b=int(input())
 c=int(input())
-print(f'AC= {abs(c-a)}')
-print(f'BC= {abs(c-b)}')
-print(f'AC+AB+CB= {abs(b-a)+abs(c-a)+abs(c-b)}')
+print(f'Volume = {a*b*c}')
+print(f'Surface area = {2*(a*b+b*c+a*c)}')
+
 
 
