d014df192c685ff4a6f1438905a34c567fe635e0
matin04 <fathuddinzodamatin@gmail.com>
2025-05-06 18:24:23 +0300

    1.4.10

--- w.kh.py
+++ w.kh.py
@@ -1,11 +1,9 @@
-# test1.4.9 /////////////////////////
+# test1.4.10 /////////////////////////
 
 
 a=int(input())
-b=int(input())
-c=int(input())
-print(f'Volume = {a*b*c}')
-print(f'Surface area = {2*(a*b+b*c+a*c)}')
+print(a/100)
+
 
 
 
