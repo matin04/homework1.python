88138e68e41553d64f56b4058f8cdfd735043d97
matin04 <fathuddinzodamatin@gmail.com>
2025-05-06 18:31:26 +0300

    1.4.13

--- w.kh.py
+++ w.kh.py
@@ -1,10 +1,17 @@
-# test1.4.12 /////////////////////////
+# test1.4.13 /////////////////////////
 
 
 a=int(input())
-b=int(input())
-c=int(input())
-print((a+b+c)//3)
+print(f'{a} * {1} = {a*1}')
+print(f'{a} * {2} = {a*2}')
+print(f'{a} * {3} = {a*3}')
+print(f'{a} * {4} = {a*4}')
+print(f'{a} * {5} = {a*5}')
+print(f'{a} * {6} = {a*6}')
+print(f'{a} * {7} = {a*7}')
+print(f'{a} * {8} = {a*8}')
+print(f'{a} * {9} = {a*9}')
+print(f'{a} * {10} = {a*10}')
 
 
 
