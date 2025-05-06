aae1d0cf63a3c4a45b830e08b34dddab72d81c91
matin04 <fathuddinzodamatin@gmail.com>
2025-05-06 17:13:47 +0300

    1.3.5

--- w.kh.py
+++ w.kh.py
@@ -1,12 +1,9 @@
-# test1.3.4 /////////////////////////
+# test1.3.5 /////////////////////////
 
 a=int(input())
-b=a//10000
-c=a//1000%10
-d=a//100%10
-e=a//10%10
-f=a%10
-print(f'The sum of the digits is : {b}+{c}+{d}+{e}+{f}={b+c+d+e+f}')
-print(f'The product of the digits is : {b}*{c}*{d}*{e}*{f}={b*c*d*e*f}')
-
+b=a//1000
+c=a//100%10
+d=a//10%10
+e=a%10
+print(e*1000+d*100+c*10+b)
 
