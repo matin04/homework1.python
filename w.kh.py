4ce183c2b6d10f9de10c50260a327b6f188e9886
matin04 <fathuddinzodamatin@gmail.com>
2025-05-06 18:34:47 +0300

    1.4.14

--- w.kh.py
+++ w.kh.py
@@ -1,17 +1,15 @@
-# test1.4.13 /////////////////////////
+# test1.4.14 /////////////////////////
 
 
-a=int(input())
-print(f'{a} * {1} = {a*1}')
-print(f'{a} * {2} = {a*2}')
-print(f'{a} * {3} = {a*3}')
-print(f'{a} * {4} = {a*4}')
-print(f'{a} * {5} = {a*5}')
-print(f'{a} * {6} = {a*6}')
-print(f'{a} * {7} = {a*7}')
-print(f'{a} * {8} = {a*8}')
-print(f'{a} * {9} = {a*9}')
-print(f'{a} * {10} = {a*10}')
+s=int(input())
+x=s//6
+a=2*x
+print(f'Abdullo : {x}')
+print(f'Ahmadjon : {a*2}')
+print(f'Muhammad : {x}')
+
+
+
 
 
 
