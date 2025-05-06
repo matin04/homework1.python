2cf6d905f04bb3758acc79a7080015a9ff4256b7
matin04 <fathuddinzodamatin@gmail.com>
2025-05-06 18:26:48 +0300

    1.4.11

--- w.kh.py
+++ w.kh.py
@@ -1,8 +1,12 @@
-# test1.4.10 /////////////////////////
+# test1.4.11 /////////////////////////
 
 
-a=int(input())
-print(a/100)
+n=int(input())
+a=n//100
+b=n//10%10
+c=n%10
+print(f'Last digit = {c}')
+print(f'Middle digit = {b}')
 
 
 
