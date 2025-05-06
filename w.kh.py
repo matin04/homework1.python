5846fe15f687ccd3ffd903ac256411227fd135f2
matin04 <fathuddinzodamatin@gmail.com>
2025-05-06 18:19:45 +0300

    1.4.8

--- w.kh.py
+++ w.kh.py
@@ -1,8 +1,11 @@
-# test1.4.7 /////////////////////////
+# test1.4.8 /////////////////////////
 
 
 a=int(input())
 b=int(input())
-print(((a+b-abs(a-b))//2)," ",((a+b+abs(a-b))//2))
+c=int(input())
+print(f'AC= {abs(c-a)}')
+print(f'BC= {abs(c-b)}')
+print(f'AC+AB+CB= {abs(b-a)+abs(c-a)+abs(c-b)}')
 
 
